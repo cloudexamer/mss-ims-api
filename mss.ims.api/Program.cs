85c91ce2f4efc26b37e4b5f766d11903fa05e12a
David Jacobo <1889675+djacobos@users.noreply.github.com>
2026-04-30 20:03:29 -0500

    Enable swagger in demo

--- mss.ims.api/Program.cs
+++ mss.ims.api/Program.cs
@@ -18,12 +18,16 @@ builder.Services.AddInfrastructure(builder.Configuration);
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-    //app.MapOpenApi();
-}
+//if (app.Environment.IsDevelopment())
+//{
+//    app.UseSwagger();
+//    app.UseSwaggerUI();
+//    app.MapOpenApi();
+//}
+
+app.MapGet("/", () => "MSS IMS API is running");
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
 
